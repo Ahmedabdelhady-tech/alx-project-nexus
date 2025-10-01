3e053e7937e1bf25fcc8e4d50188fabba8ec435b
ahmedabdelhady <ahmedabdelhadye@gmail.com>
2025-10-01 23:37:40 +0300

    Railway

--- jobboard/settings.py
+++ jobboard/settings.py
@@ -104,7 +104,11 @@ AUTH_USER_MODEL = "users.CustomUser"
 
 import dj_database_url
 
-DATABASES = {"default": dj_database_url.config(default=os.getenv("DATABASE_URL"))}
+DATABASES = {
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL"), conn_max_age=600, ssl_require=True
+    )
+}
 
 
 REST_FRAMEWORK = {
@@ -159,6 +163,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
